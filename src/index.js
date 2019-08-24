55ac163b3df119acd867c62983c4e832894353f9
Alexey Borovik <ityetti@gmail.com>
2019-08-24 23:48:50 +0300

    Homework of the first week

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(name) {
+    return name;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b = 100) {
+    return a + b;
 }
 
 /*
@@ -41,6 +43,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn();
 }
 
 /*
@@ -57,6 +60,11 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 13
  */
 function returnCounter(number) {
+    number = 0;
+
+    return function f() {
+        return number++;
+    }
 }
 
 /*
@@ -68,7 +76,12 @@ function returnCounter(number) {
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {
+function returnArgumentsArray(...args) {
+    if (!args) {
+        args = [];
+    }
+
+    return args;
 }
 
 /*
@@ -86,7 +99,11 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+function bindFunction(fn, ...args) {
+    return function fn() {
+        // We must set 'sum' as argument for reduce
+        return args.reduce()
+    }
 }
 
 export {
